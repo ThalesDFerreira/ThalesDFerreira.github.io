3de9fc3cb88c5a9f7317dffe5479377435d3df15
ThalesDFerreira <thalesferreira190@gmail.com>
2022-07-12 13:46:55 -0300

    feito o header, e home incompleto

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import './App.css';
-import Login from './pages/Login';
-import Portfolio from './pages/Portfolio';
+import Home from './pages/Home';
 import Projetos from './pages/Projetos';
 import Contatos from './pages/Contatos';
 import SobreMim from './pages/SobreMim';
@@ -12,8 +11,7 @@ class App extends Component {
     return (
       <BrowserRouter>
         <Switch>
-          <Route exact path="/" component={Login} />
-          <Route exact path="/portfolio" component={Portfolio} />
+          <Route exact path="/" component={Home} />
           <Route exact path="/projects" component={Projetos} />
           <Route exact path="/contacts" component={Contatos} />
           <Route exact path="/aboult" component={SobreMim} />
---
+++ src/components/Header.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Header.css';
+
+class Header extends React.Component {
+  toggleMenu = (event) => {
+    if (event.type === 'touchstart') event.preventDefault();
+    const nav = document.getElementById('nav');
+    nav.classList.toggle('active');
+    const active = nav.classList.contains('active');
+    event.currentTarget.setAttribute('aria-expanded', active);
+    if (active) {
+      event.currentTarget.setAttribute('aria-label', 'Fechar Menu');
+    } else {
+      event.currentTarget.setAttribute('aria-label', 'Abrir Menu');
+    }
+  }
+
+  render() {
+    return (
+      <header id="header">
+        <h3>Portfólio Dev</h3>
+        <nav id="nav">
+          <button aria-label="Abrir Menu" id="btn-mobile" aria-haspopup="true" aria-controls="menu" aria-expanded="false" onClick={this.toggleMenu}>Menu
+            <span id="hamburger" />
+          </button>
+          <ul id="menu" role="menu">
+            <li><Link className="link-li" id="link1" to="/">Início</Link></li>
+            <li><Link className="link-li" id="link2" to="/projects">Projetos</Link></li>
+            <li><Link className="link-li" id="link3" to="/contacts">Contatos</Link></li>
+            <li><Link className="link-li" id="link4" to="/aboult">Sobre mim</Link></li>
+          </ul>
+        </nav>
+      </header >
+    );
+  }
+}
+
+export default Header;
