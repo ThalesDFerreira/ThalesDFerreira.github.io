f12bb4de644cad3a2ce9602895160c64aa9c0af8
ThalesDFerreira <thalesferreira190@gmail.com>
2022-07-13 13:25:36 -0300

    finalizado

--- src/pages/Projetos.js
+++ src/pages/Projetos.js
@@ -14,34 +14,26 @@ class Projetos extends React.Component {
             <h1 className="text">Principais Projetos</h1>
           </div>
           <div>
-            <ul>
               <div className="div-container-projects">
                 <div className="links-container">
-                  <li>
                     <a style={{ textDecoration: 'none' }} className="link" href="https://github.com/tryber/sd-021-b-project-trivia-react-redux">
                       <img className="image-link" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original-wordmark.svg" alt="GitHub" />
                       <h2>Projeto Trivia</h2>
                     </a>
-                  </li>
                 </div>
                 <div className="links-container">
-                  <li>
                     <a style={{ textDecoration: 'none' }} className="link" href="https://github.com/tryber/sd-021-b-project-frontend-online-store">
                       <img className="image-link" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original-wordmark.svg" alt="GitHub" />
                       <h2>Projeto Frontend<br />Online Store</h2>
                     </a>
-                  </li>
                 </div>
                 <div className="links-container">
-                  <li>
                     <a style={{ textDecoration: 'none' }} className="link" href="https://github.com/tryber/sd-021-b-project-trybewallet">
                       <img className="image-link" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original-wordmark.svg" alt="GitHub" />
                       <h2>Projeto<br />TrybeWallet</h2>
                     </a>
-                  </li>
                 </div>
               </div>
-            </ul>
           </div>
 
         </main>
