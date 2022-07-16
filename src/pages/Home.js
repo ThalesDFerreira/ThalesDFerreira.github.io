f6b2e68522e3f81b3ff490adfd31850292c263fa
ThalesDFerreira <thalesferreira190@gmail.com>
2022-07-16 20:30:23 -0300

    Modificações na responsividade home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -9,31 +9,33 @@ class Home extends React.Component {
       <div>
         <Header />
         <main className="container-main">
-          <div className="col-lg-12" id="container-div-main">
-            <div>
-              <h2 id="text1">Olá, muito prazer!</h2>
-            </div>
-            <div>
-              <h2 id="text2">Sou Desenvolvedor de Software !</h2>
-            </div>
-            <div>
-              <h2 id="text3">Meu nome é Thales Ferreira e esse é meu portfólio.</h2>
-            </div>
-            <div className="div-container-redirects">
-              <div className="links-redirects">
-                <Link to="/projects">
-                  <button type="button" id="btn-works">Trabalhos (GitHub)</button>
-                </Link>
+          <div className="container-home">
+            <div id="container-div-main">
+              <div>
+                <h2 id="text1">Olá, muito prazer!</h2>
               </div>
-              <div className="links-redirects">
-                <Link to="/aboult">
-                  <button type="button" id="btn-aboult">Sobre Mim</button>
-                </Link>
+              <div>
+                <h2 id="text2">Sou Desenvolvedor de Software !</h2>
               </div>
-              <div className="links-redirects">
-                <Link to="/contacts">
-                  <button type="button" id="btn-contacts">Contatos</button>
-                </Link>
+              <div>
+                <h2 id="text3">Meu nome é Thales Ferreira e esse é meu portfólio.</h2>
+              </div>
+              <div className="div-container-redirects">
+                <div className="links-redirects">
+                  <Link to="/projects">
+                    <button type="button" id="btn-works">Trabalhos (GitHub)</button>
+                  </Link>
+                </div>
+                <div className="links-redirects">
+                  <Link to="/aboult">
+                    <button type="button" id="btn-aboult">Sobre Mim</button>
+                  </Link>
+                </div>
+                <div className="links-redirects">
+                  <Link to="/contacts">
+                    <button type="button" id="btn-contacts">Contatos</button>
+                  </Link>
+                </div>
               </div>
             </div>
           </div>
