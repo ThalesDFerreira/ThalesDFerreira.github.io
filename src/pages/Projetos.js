d91b983e082bb1107967b92936c26b0e46a22cac
ThalesDFerreira <thalesferreira190@gmail.com>
2022-08-14 16:03:14 -0300

    add link de aplicação funcionando (deploy)

--- src/pages/Projetos.js
+++ src/pages/Projetos.js
@@ -15,21 +15,30 @@ class Projetos extends React.Component {
             <div>
               <div className="div-container-projects">
                 <div className="links-container">
-                  <a style={{ textDecoration: 'none' }} className="link" href="https://github.com/ThalesDFerreira/Projeto-Trivia">
+                  <a style={{ textDecoration: 'none' }} className="link" href="https://playful-frangollo-ecde6a.netlify.app/">
                     <img className="image-link" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original-wordmark.svg" alt="GitHub" />
                     <h2>Projeto Trivia</h2>
                   </a>
-                </div>
-                <div className="links-container">
-                  <a style={{ textDecoration: 'none' }} className="link" href="https://github.com/ThalesDFerreira/Projeto-Frontend-Online-Store">
-                    <img className="image-link" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original-wordmark.svg" alt="GitHub" />
-                    <h2>Projeto Frontend<br />Online Store</h2>
+                  <a style={{ textDecoration: 'none' }} className="link" href="https://github.com/ThalesDFerreira/Projeto-Trivia">
+                    <h2>Código</h2>
                   </a>
                 </div>
                 <div className="links-container">
-                  <a style={{ textDecoration: 'none' }} className="link" href="https://github.com/ThalesDFerreira/Projeto-TrybeWallet">
+                  <a style={{ textDecoration: 'none' }} className="link" href="https://tasty-food-chi.vercel.app/">
+                    <img className="image-link" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original-wordmark.svg" alt="GitHub" />
+                    <h2>Projeto App de Receitas</h2>
+                  </a>
+                  <a style={{ textDecoration: 'none' }} className="link" href="https://github.com/ThalesDFerreira/Projeto-Frontend-Online-Store">
+                    <h2>Código</h2>
+                  </a>
+                </div>
+                <div className="links-container">
+                  <a style={{ textDecoration: 'none' }} className="link" href="https://glittery-wisp-b83799.netlify.app/">
                     <img className="image-link" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original-wordmark.svg" alt="GitHub" />
                     <h2>Projeto<br />TrybeWallet</h2>
+                  </a>
+                  <a style={{ textDecoration: 'none' }} className="link" href="https://github.com/ThalesDFerreira/Projeto-TrybeWallet">
+                    <h2>Código</h2>
                   </a>
                 </div>
               </div>
