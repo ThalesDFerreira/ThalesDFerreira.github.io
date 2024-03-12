fe26c1164a75a552216e90951cee200498caf07e
ThalesDFerreira <thalesferreira190@gmail.com>
2024-03-12 18:44:43 -0300

    feat: add novos projetos

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,59 +5,86 @@ import LogoLinkedin from '../assets/linkedin.png';
 import LogoGitHub from '../assets/github.png';
 import LogoFacebook from '../assets/facebook.png';
 import LogoGmail from '../assets/gmail.png';
-import "./Home.css";
+import './Home.css';
 
 class Home extends React.Component {
   render() {
     return (
       <div>
-        <main className="container-main-base">
-          <div className="div-container-contatos1">
-            <div className="links-contatos item-link">
-              <a className="link-logo" href="https://api.whatsapp.com/send?phone=5516992746725" target="_blank" rel="noopener noreferrer">
-                <img className="image-logo" src={LogoWhatsApp} alt="WhatsApp" />
+        <main className='container-main-base'>
+          <div className='div-container-contatos1'>
+            <div className='links-contatos item-link'>
+              <a
+                className='link-logo'
+                href='https://api.whatsapp.com/send?phone=5516992746725'
+                target='_blank'
+                rel='noopener noreferrer'
+              >
+                <img className='image-logo' src={LogoWhatsApp} alt='WhatsApp' />
               </a>
             </div>
-            <div className="links-contatos">
-              <a className="link-logo" href="https://www.linkedin.com/in/thales-david-ferreira-a47378107/" target="_blank" rel="noopener noreferrer">
-                <img className="image-logo" src={LogoLinkedin} alt="LinkedIn" />
+            <div className='links-contatos'>
+              <a
+                className='link-logo'
+                href='https://www.linkedin.com/in/thales-david-ferreira-a47378107/'
+                target='_blank'
+                rel='noopener noreferrer'
+              >
+                <img className='image-logo' src={LogoLinkedin} alt='LinkedIn' />
               </a>
             </div>
-            <div className="links-contatos">
-              <a className="link-logo" href="https://github.com/ThalesDFerreira/" target="_blank" rel="noopener noreferrer">
-                <img className="image-logo" src={LogoGitHub} alt="GitHub" />
+            <div className='links-contatos'>
+              <a
+                className='link-logo'
+                href='https://github.com/ThalesDFerreira/'
+                target='_blank'
+                rel='noopener noreferrer'
+              >
+                <img className='image-logo' src={LogoGitHub} alt='GitHub' />
               </a>
             </div>
-            <div className="links-contatos">
-              <a className="link-logo" href="https://www.facebook.com/thalesferreira190/" target="_blank" rel="noopener noreferrer">
-                <img className="image-logo" src={LogoFacebook} alt="Facebook" />
+            <div className='links-contatos'>
+              <a
+                className='link-logo'
+                href='https://www.facebook.com/thalesferreira190/'
+                target='_blank'
+                rel='noopener noreferrer'
+              >
+                <img className='image-logo' src={LogoFacebook} alt='Facebook' />
               </a>
             </div>
-            <div className="links-contatos">
-              <a className="link-logo" href="mailto:thalesferreira190@gmail.com/" target="_blank" rel="noopener noreferrer">
-                <img className="image-logo" src={LogoGmail} alt="Gmail" />
+            <div className='links-contatos'>
+              <a
+                className='link-logo'
+                href='mailto:thalesferreira190@gmail.com/'
+                target='_blank'
+                rel='noopener noreferrer'
+              >
+                <img className='image-logo' src={LogoGmail} alt='Gmail' />
               </a>
             </div>
           </div>
-          <div className="container-home">
-            <div id="container-div-main">
+          <div className='container-home'>
+            <div id='container-div-main'>
               <div>
-                <h2 id="text1">Olá, eu sou Thales.</h2>
+                <h2 id='text1'>Olá, eu sou Thales.</h2>
               </div>
               <div>
-                <h2 id="text2">Muito prazer ! Sou Desenvolvedor de Software.</h2>
+                <h2 id='text2'>
+                  Muito prazer ! Sou Desenvolvedor de Software.
+                </h2>
               </div>
-              <div className="div-container-redirects">
-                <div className="links-redirects">
-                  <Link to="/projects">
-                    <button type="button" className="btn-works">
+              <div className='div-container-redirects'>
+                <div className='links-redirects'>
+                  <Link to='/projects'>
+                    <button type='button' className='btn-works'>
                       PORTFÓLIO
                     </button>
                   </Link>
                 </div>
-                <div className="links-redirects">
-                  <Link to="/aboult">
-                    <button type="button" className="btn-works">
+                <div className='links-redirects'>
+                  <Link to='/aboult'>
+                    <button type='button' className='btn-works'>
                       SOBRE MIM
                     </button>
                   </Link>
--- src/pages/Projetos.js
+++ src/pages/Projetos.js
@@ -1,59 +1,173 @@
 import React from 'react';
 import Header from '../components/Header';
-import "./Projetos.css";
+import './Projetos.css';
 
 class Projetos extends React.Component {
   render() {
     return (
       <>
         <Header />
-        <main className="container-main">
-          <div className="container-projetos">
+        <main className='container-main'>
+          <div className='container-projetos'>
             <div>
-              <h1 className="text">Principais Projetos</h1>
+              <h1 className='text'>Principais Projetos</h1>
             </div>
             <div>
-              <div className="div-container-projects">
-                <div className="links-container">
-                  <a style={{ textDecoration: 'none' }} className="link" href="https://playful-frangollo-ecde6a.netlify.app/">
-                    <img className="image-link" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original-wordmark.svg" alt="GitHub" />
+              <div className='div-container-projects'>
+                <div className='links-container'>
+                  <a
+                    style={{ textDecoration: 'none' }}
+                    className='link'
+                    href='https://playful-frangollo-ecde6a.netlify.app/'
+                  >
+                    <img
+                      className='image-link'
+                      src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original-wordmark.svg'
+                      alt='GitHub'
+                    />
                     <h2>Projeto Trivia</h2>
                   </a>
-                  <a style={{ textDecoration: 'none' }} className="link" href="https://github.com/ThalesDFerreira/Projeto-Trivia">
+                  <a
+                    style={{ textDecoration: 'none' }}
+                    className='link'
+                    href='https://github.com/ThalesDFerreira/Projeto-Trivia'
+                  >
                     <h2>Código</h2>
                   </a>
                 </div>
-                <div className="links-container">
-                  <a style={{ textDecoration: 'none' }} className="link" href="https://tasty-food-chi.vercel.app/">
-                    <img className="image-link" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original-wordmark.svg" alt="GitHub" />
+                <div className='links-container'>
+                  <a
+                    style={{ textDecoration: 'none' }}
+                    className='link'
+                    href='https://tasty-food-chi.vercel.app/'
+                  >
+                    <img
+                      className='image-link'
+                      src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original-wordmark.svg'
+                      alt='GitHub'
+                    />
                     <h2>Projeto App de Receitas</h2>
                   </a>
-                  <a style={{ textDecoration: 'none' }} className="link" href="https://github.com/ThalesDFerreira/Projeto-Frontend-Online-Store">
+                  <a
+                    style={{ textDecoration: 'none' }}
+                    className='link'
+                    href='https://github.com/ThalesDFerreira/Projeto-Frontend-Online-Store'
+                  >
                     <h2>Código</h2>
                   </a>
                 </div>
-                <div className="links-container">
-                  <a style={{ textDecoration: 'none' }} className="link" href="https://glittery-wisp-b83799.netlify.app/">
-                    <img className="image-link" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original-wordmark.svg" alt="GitHub" />
-                    <h2>Projeto<br />TrybeWallet</h2>
+                <div className='links-container'>
+                  <a
+                    style={{ textDecoration: 'none' }}
+                    className='link'
+                    href='https://glittery-wisp-b83799.netlify.app/'
+                  >
+                    <img
+                      className='image-link'
+                      src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original-wordmark.svg'
+                      alt='GitHub'
+                    />
+                    <h2>
+                      Projeto
+                      <br />
+                      TrybeWallet
+                    </h2>
                   </a>
-                  <a style={{ textDecoration: 'none' }} className="link" href="https://github.com/ThalesDFerreira/Projeto-TrybeWallet">
+                  <a
+                    style={{ textDecoration: 'none' }}
+                    className='link'
+                    href='https://github.com/ThalesDFerreira/Projeto-TrybeWallet'
+                  >
                     <h2>Código</h2>
                   </a>
                 </div>
-                <div className="links-container">
-                  <a style={{ textDecoration: 'none' }} className="link" href="https://ng-cash-frontend-23v2c26xp-thalesdferreira.vercel.app/">
-                    <img className="image-link" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original-wordmark.svg" alt="GitHub" />
-                    <h2>Projeto<br />NGCash</h2>
+                <div className='links-container'>
+                  <a
+                    style={{ textDecoration: 'none' }}
+                    className='link'
+                    href='https://ng-cash-frontend-23v2c26xp-thalesdferreira.vercel.app/'
+                  >
+                    <img
+                      className='image-link'
+                      src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original-wordmark.svg'
+                      alt='GitHub'
+                    />
+                    <h2>
+                      Projeto
+                      <br />
+                      NGCash
+                    </h2>
                   </a>
-                  <a style={{ textDecoration: 'none' }} className="link" href="https://github.com/ThalesDFerreira/NGCash">
+                  <a
+                    style={{ textDecoration: 'none' }}
+                    className='link'
+                    href='https://github.com/ThalesDFerreira/NGCash'
+                  >
                     <h2>Código</h2>
                   </a>
                 </div>
-                <div className="links-container">
-                  <a style={{ textDecoration: 'none' }} className="link" href="https://github.com/ThalesDFerreira/Projeto-App-Delivery">
-                    <img className="image-link" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original-wordmark.svg" alt="GitHub" />
-                    <h2>Projeto<br />App Delivery</h2>
+                <div className='links-container'>
+                  <a
+                    style={{ textDecoration: 'none' }}
+                    className='link'
+                    href='https://github.com/ThalesDFerreira/Projeto-App-Delivery'
+                  >
+                    <img
+                      className='image-link'
+                      src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original-wordmark.svg'
+                      alt='GitHub'
+                    />
+                    <h2>
+                      Projeto
+                      <br />
+                      App Delivery
+                    </h2>
+                  </a>
+                </div>
+              </div>
+              <div className='div-container-projects'>
+                <div className='links-container'>
+                  <a
+                    style={{ textDecoration: 'none' }}
+                    className='link'
+                    href='https://github.com/ThalesDFerreira/Desafio-FullStack-Facilita-Jur-dico'
+                  >
+                    <img
+                      className='image-link'
+                      src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original-wordmark.svg'
+                      alt='GitHub'
+                    />
+                    <h2>Desafio FullStack Facilita Jurídico</h2>
+                  </a>
+                </div>
+                <div className='links-container'>
+                  <a
+                    style={{ textDecoration: 'none' }}
+                    className='link'
+                    href='https://github.com/ThalesDFerreira/desafio-fullstack'
+                  >
+                    <img
+                      className='image-link'
+                      src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original-wordmark.svg'
+                      alt='GitHub'
+                    />
+                    <h2>Desafio FullStack Telegram</h2>
+                  </a>
+                </div>
+                <div className='links-container'>
+                  <a
+                    style={{ textDecoration: 'none' }}
+                    className='link'
+                    href='https://github.com/ThalesDFerreira/Front-HVEX'
+                  >
+                    <img
+                      className='image-link'
+                      src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original-wordmark.svg'
+                      alt='GitHub'
+                    />
+                    <h2>
+                    Frontend HVEX
+                    </h2>
                   </a>
                 </div>
               </div>
